0087d05fb567bbc71a931456f706414e0be18a72
MANISH KUMAR <121341863+manishks1988@users.noreply.github.com>
2022-12-24 17:35:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('I started work on Python..!')
 streamlit.header('My observation')
-streamlit.Text('Also happy to do conding in this App')
+streamlit.text('Also happy to do conding in this App')
 streamlit.text('its still easy to do code')
 stramlit.text('Now we can see next code to add..!')
