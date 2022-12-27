b2984f143615679ee90afcc1b3ee7b9c14c23c83
MANISH KUMAR <121341863+manishks1988@users.noreply.github.com>
2022-12-27 22:49:20 +0530

    Update streamlit_app.py
    To show account Details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 
-
 streamlit.title('My Moms New healthy diner')
 streamlit.header('Breakfast favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -30,10 +29,16 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
-
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
